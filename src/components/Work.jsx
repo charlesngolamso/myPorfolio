1c3617e01056b11ffc9c5cba4ea0a857f130ad79
Charles So <charlessoworkacc@gmail.com>
2022-10-01 13:20:30 +0800

    updated links

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -23,10 +23,10 @@ const Work = () => {
                         </span>
 
                         <div className='pt-8 text-center'>
-                            <a href='/'>
+                            <a href='https://game-of-life-i7xm-charlesngolamso.vercel.app/'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href='/'>
+                            <a href='https://github.com/charlesngolamso/game-of-life'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
